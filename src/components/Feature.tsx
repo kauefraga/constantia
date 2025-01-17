2021730fd99fa2303af720042dc6967d45389bc8
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2025-01-17 16:50:13 -0300

    Use tagged template literals for <FeatureCard> styles

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,20 +1,23 @@
 import type { ReactElement, ReactNode } from "react";
 import styled from "styled-components";
 
-const Container = styled.div({
-  border: "1px solid rgb(0 0 0 / 0.2)",
-  borderRadius: 4,
-  boxShadow: "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)", // tailwind shadow-md
-  maxWidth: 344,
-  padding: "0 8px",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  gap: 16,
-  "*": {
-    margin: 0,
-  },
-});
+const Container = styled.div`
+  border: 1px solid rgb(0 0 0 / 0.2);
+  border-radius: 4px;
+  box-shadow:
+    0 4px 6px -1px rgb(0 0 0 / 0.1),
+    0 2px 4px -2px rgb(0 0 0 / 0.1); /* tailwind shadow-md */
+  max-width: 344px;
+  padding: 0 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 16px;
+
+  * {
+    margin: 0;
+  }
+`;
 
 interface FeatureCardProps {
   children: ReactNode;
